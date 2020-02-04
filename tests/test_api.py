4fdd76dd1ef368134d3678ace86816d104249734
Nikos Fotiou <nikosft@gmail.com>
2020-02-04 23:39:01 +0200

    new test for the REST API

---
+++ tests/test_api.py
@@ -0,0 +1,26 @@
+from IAA import iaa
+import pytest
+import requests
+import json
+
+@pytest.fixture(autouse=True)
+def server():
+    import subprocess
+    import time
+    p1 = subprocess.Popen(['python3', 'IAA/iaa.py'])
+    time.sleep(1) #Otherwise the server is not ready when tests start
+    yield
+    p1.kill()
+
+
+def test_valid_bearer():
+    payload = {'token-type':'Bearer'}
+    response  = requests.post("http://localhost:9000/verifytoken", data = payload).text
+    response =json.loads(response)
+    assert(response['result'] == "Success")
+
+def test_invalid_token_type():
+    payload = {'token-type':'Garbage'}
+    response  = requests.post("http://localhost:9000/verifytoken", data = payload).text
+    response =json.loads(response)
+    assert(response['result'] == "Failure")
